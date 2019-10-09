692a79579bb3ffc24f38cc1a5d13c7e771a2a8ab
Ana Rocha <apdsrocha@gmail.com>
2019-10-09 19:08:56 -0300

    added exercise 02

---
+++ js/exercises.js
@@ -0,0 +1,49 @@
+/*1º exercício
+Crie um botão que ao ser clicado cria um novo elemento em tela com a forma de um quadrado
+vermelho com 100px de altura e largura. Sempre que o botão for clicado um novo quadrado deve
+aparecer na tela.
+
+2º exercício
+Utilizando o resultado do primeiro desafio, toda vez que o usuário passar o mouse por cima de
+algum quadrado troque sua cor para uma cor aleatória gerada pela função abaixo:
+function getRandomColor() {
+ var letters = "0123456789ABCDEF";
+ var color = "#";
+ for (var i = 0; i < 6; i++) {
+ color += letters[Math.floor(Math.random() * 16)];
+ }
+ return color;
+}
+var newColor = getRandomColor(); // #E943F0 */
+
+let btnCreate = document.createElement('button');
+let btnDiv = document.querySelector('.btn-div');
+
+let createBox = function() {
+    let boxResults = document.querySelector('.box-results');
+    let newBox = document.createElement('div');
+    newBox.style.width = "100px";
+    newBox.style.height = "100px";
+    newBox.style.backgroundColor = "red";
+    console.log(newBox.style.backgroundColor)
+    newBox.style.margin = "2%";
+    newBox.onmouseover = function() {
+        newBox.style.backgroundColor = getRandomColor();
+    };
+    console.log(newBox.style.backgroundColor)
+    boxResults.appendChild(newBox);
+}
+
+btnCreate.innerText = "Click to make a box, I guess";
+btnCreate.addEventListener('click', createBox);
+
+btnDiv.appendChild(btnCreate)
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+   }
